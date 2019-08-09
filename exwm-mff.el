cbf88abcf21a50c93336865764aafcf0cbc1594c
Ian Eure <ian@retrospec.tv>
2019-08-09 16:08:36 -0700

    Cut v1.0.4 release.

--- exwm-mff.el
+++ exwm-mff.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ian Eure <public@lowbar.fyi>
 ;; URL: https://github.com/ieure/exwm-mff
-;; Version: 1.0.3
+;; Version: 1.0.4
 ;; Package-Requires: ((emacs "25") (exwm "0.22.1"))
 ;; Keywords: unix
 
